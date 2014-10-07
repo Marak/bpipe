06e6bb743ba9169712c94f812e3ddbfe252793b4
Marak <marak.squires@gmail.com>
2014-10-07 13:11:33 +0200

    [api] [refactor] Moved dom selector logic into separate module. Transform pipes can now be programmatically added to bpipe server and client through simple require system #4

--- bundle.js
+++ bundle.js
@@ -7,83 +7,82 @@ var bpipes = {};
 var dataplex = require('dataplex');
 var domstream = require('domnode-dom');
 
-var plex = dataplex();
+function openPipe () {
 
-// create connection to websocket server
-var con = ws('ws://localhost:8001');
-var stream = plex.open('/commands');
+  var plex = dataplex();
 
-/*
-var stream2 = plex.open('/foo');
-stream2.on('error', function(err){
-  console.log('stream2 error', err);
-});
-stream2.pipe(through(function(chunk, enc, file){
-  console.log('got foo data');
-}));
-*/
+  // create connection to websocket server
+  var con = ws('ws://localhost:8001');
+  var stream = plex.open('/querySelector');
 
-con.pipe(plex).pipe(con);
+  con.pipe(plex).pipe(con);
 
-stream.on('error', function(err){
-  console.log('stream error', err);
-});
+  stream.on('error', function(err){
+    console.log('stream error', err);
+  });
 
-con.on('error', function(err){
-  console.log('con error', err);
-});
+  con.on('error', function(err){
+    console.log('con error', err);
+  });
 
-var hello = {
-  "source": "browser",
-  "id": new Date().getTime(),
-  "url": window.location.href
+  var hello = {
+    "source": "browser",
+    "id": new Date().getTime(),
+    "url": window.location.href
+  };
+
+  stream.write(new Buffer(JSON.stringify(hello)))
+
+  return stream;
 };
 
-stream.write(new Buffer(JSON.stringify(hello)))
+function handleStream (stream) {
+  stream.on('data', function(x) { 
 
-stream.on('data', function(x) { 
+    // console.log('data from server', x.toString());
 
-  // console.log('data from server', x.toString());
+    var data = JSON.parse(x.toString());
+    var selector = data.selector;
 
-  var data = JSON.parse(x.toString());
-  var selector = data.selector;
-
-  if (typeof selector === "undefined" || selector.length === 0) {
-    selector = "body";
-  }
-
-  // if there is no data, assume its a request to bind a new event listerer from bpipe
-  if (typeof data.data === "undefined") {
-
-    // if event is already bound, don't re-bind it
-    // TODO: instead, delete it and re-bind
-    if (typeof bpipes[selector] === "object") {
-      return;
+    if (typeof selector === "undefined" || selector.length === 0) {
+      selector = "body";
     }
 
-    bpipes[selector] = { inputStream: inputStream };
+    // if there is no data, assume its a request to bind a new event listerer from bpipe
+    if (typeof data.data === "undefined") {
 
-    var inputElement =  document.querySelectorAll(selector)[0]; // TODO: forEach on the elements, instead of just acting on first matching element
-    var inputStream = domstream.createEventStream(inputElement, data.event);
-    // console.log('mapping new element', inputElement, selector)
-    inputStream.pipe(through.obj(function(data, enc, cb){
-      var data = {
-        "selector": selector,
-        "data": data,
-        "source": "browser",
-        "url": window.location.href
-      };
-      cb(null, JSON.stringify(data));
-    }))
-    .pipe(stream);
-  } else {
-    // data incoming from bpipe, send it to the browser
-    var outputElement = document.querySelectorAll(selector)[0];
-    var outputStream =  domstream.createWriteStream(outputElement, 'text/plain');
-    // console.log('incoming data from bpipe', data, outputElement);
-    outputStream.write(data.data);
-  }
-});
+      // if event is already bound, don't re-bind it
+      // TODO: instead, delete it and re-bind
+      if (typeof bpipes[selector] === "object") {
+        return;
+      }
+
+      bpipes[selector] = { inputStream: inputStream };
+
+      var inputElement =  document.querySelectorAll(selector)[0]; // TODO: forEach on the elements, instead of just acting on first matching element
+      var inputStream = domstream.createEventStream(inputElement, data.event);
+      // console.log('mapping new element', inputElement, selector)
+      inputStream.pipe(through.obj(function(data, enc, cb){
+        var data = {
+          "selector": selector,
+          "data": data,
+          "source": "browser",
+          "url": window.location.href
+        };
+        cb(null, JSON.stringify(data));
+      }))
+      .pipe(stream);
+    } else {
+      // data incoming from bpipe, send it to the browser
+      var outputElement = document.querySelectorAll(selector)[0];
+      var outputStream =  domstream.createWriteStream(outputElement, 'text/plain');
+      // console.log('incoming data from bpipe', data, outputElement);
+      outputStream.write(data.data);
+    }
+  });
+};
+
+handleStream(openPipe());
 }).call(this,require("buffer").Buffer)
 },{"buffer":85,"dataplex":2,"domnode-dom":42,"through2":56,"websocket-stream":57}],2:[function(require,module,exports){
 (function (Buffer){
---
+++ examples/server.js
@@ -0,0 +1,15 @@
+var server = require('../lib/server');
+
+server.start({ port: 8001 }, function (err, server){
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  server.on('wsconnection', function (stream){
+    console.log('got ws connection');
+    stream.plex.add('/querySelector', require('../lib/pipes/querySelector')());
+  });
+
+  console.log('bpipe ws server started on port ' + server.address().port);
+});
